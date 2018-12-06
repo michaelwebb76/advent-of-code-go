682297e486cbcc6414ba9d9cafa33f80ddd72b74
Michael Webb <moggyboy@gmail.com>
2018-12-06 11:53:10 +1100

    Completed second part of day one

--- day_1/day_1.go
+++ day_1/day_1.go
@@ -9,6 +9,8 @@ import (
 	"strconv"
 )
 
+const limit = 32768
+
 func check(e error) {
 	if e != nil {
 		panic(e)
@@ -21,7 +23,7 @@ func fileToIntArray(filename string) []int {
 	defer f.Close()
 
 	reader := bufio.NewReader(f)
-	intArray := make([]int, 0, 32768)
+	intArray := make([]int, 0, limit)
 
 	for err == nil {
 		line, _, err := reader.ReadLine()
@@ -44,7 +46,8 @@ func fileToIntArray(filename string) []int {
 	return intArray
 }
 
-func sumFrequency(intArray []int) int {
+// SumFrequency ...
+func SumFrequency(intArray []int) int {
 	frequency := 0
 	for _, intValue := range intArray {
 		frequency += intValue
@@ -52,10 +55,43 @@ func sumFrequency(intArray []int) int {
 	return frequency
 }
 
+func contains(intArray []int, targetValue int) bool {
+	for _, intValue := range intArray {
+		if intValue == targetValue {
+			return true
+		}
+	}
+	return false
+}
+
+// RepeatedFrequency ...
+func RepeatedFrequency(intArray []int) *int {
+	frequency := 0
+	pastFrequencies := make([]int, 0, limit)
+	pastFrequencies = append(pastFrequencies, frequency)
+	for i := 1; i <= limit; i++ {
+		for _, intValue := range intArray {
+			frequency += intValue
+			if contains(pastFrequencies, frequency) == true {
+				return &frequency
+			}
+			pastFrequencies = append(pastFrequencies, frequency)
+		}
+	}
+	return nil
+}
+
 func main() {
 	flag.Parse()
 
 	filename := flag.Arg(0)
 	intArray := fileToIntArray(filename)
-	fmt.Println("Sum frequency is: " + strconv.Itoa(sumFrequency(intArray)))
+	fmt.Println("Sum frequency is: " + strconv.Itoa(SumFrequency(intArray)))
+
+	repeatedFrequency := RepeatedFrequency(intArray)
+	if repeatedFrequency == nil {
+		fmt.Println("Repeated frequency is: NOT FOUND")
+	} else {
+		fmt.Println("Repeated frequency is: " + strconv.Itoa(*repeatedFrequency))
+	}
 }
